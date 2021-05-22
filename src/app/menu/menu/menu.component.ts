06bce280688ede67dea404361b6a8d46f1da82a0
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-05-22 16:50:00 +0200

    Preliminary steps for projects component

---
+++ src/app/menu/menu/menu.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  menuItems: MenuItem[] = [];
+  minifiedMenuItems: MenuItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Main',
+        items: [
+          {label: 'Dashboard', icon: PrimeIcons.HOME, routerLink: ['/main/dashboard']},
+          {
+            label: 'Times',
+            icon: PrimeIcons.CALENDAR,
+            routerLink: ['/main/times']
+          },
+          {
+            label: 'Timesheet',
+            icon: PrimeIcons.CLOCK,
+            routerLink: ['/main/timesheet'],
+          },
+          {label: 'Projects', icon: PrimeIcons.CHECK_SQUARE, routerLink: ['/main/projects']},
+          {label: 'Settings', icon: PrimeIcons.COG, routerLink: ['/main/settings']},
+        ]
+      }
+    ];
+
+    this.menuItems.forEach((item: MenuItem) => {
+      item.items?.forEach((nestedItem: MenuItem) => {
+        this.minifiedMenuItems.push({icon: nestedItem.icon, routerLink: nestedItem.routerLink});
+      });
+    });
+  }
+}
