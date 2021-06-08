cc57ef418bc283cc8603acdd7335b2525155593b
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-06-08 12:03:57 +0200

    Result from day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMenuModule } from './menu/app-menu.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AppMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
